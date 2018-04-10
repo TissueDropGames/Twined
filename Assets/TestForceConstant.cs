108a571b4f1029f66ec25e73f670b89414dcea65
Unknown <fredriklovlie@gmail.com>
2018-04-10 11:03:37 +0200

    dfsa

---
+++ Assets/TestForceConstant.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestForceConstant : MonoBehaviour
+{
+    public float fConstantForce;
+
+    private Rigidbody2D RB;
+
+    private GameObject OtherEntity;
+	// Use this for initialization
+	void Start ()
+	{
+	    RB = GetComponent<Rigidbody2D>();
+	    if (name == "Player1")
+	    {
+	        OtherEntity = GameObject.Find("Player2");
+
+        }
+	    else
+	    {
+	        OtherEntity = GameObject.Find("Player1");
+	    }
+    }
+	
+	// Update is called once per frame
+	void Update ()
+	{
+        
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Vector2 TowardsOther = transform.position - OtherEntity.transform.position;
+        TowardsOther.Normalize();
+        RB.AddForce(TowardsOther * fConstantForce);
+    }
+}
